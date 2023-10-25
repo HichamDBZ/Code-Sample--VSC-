28b79800399ac27c0b4d37cb621a4d5d3eddbd5f
HichamDBZ <147610208+HichamDBZ@users.noreply.github.com>
2023-10-25 10:24:51 +0200

    first commit

---
+++ CodeSample1.py
@@ -0,0 +1,13 @@
+a = input(str("Que souhaitez vous acheter ? "))
+b = a.lower()
+
+#Prix de chaque aliment
+PrixPomme = int(0,30)
+PrixPoire = int(0,30)
+PrixGrenade = int(1,29)
+PrixBanane = int(0,)
+
+
+if a == "pommes" : 
+    Quantite = input(int("Combien souhaitez-vous en acheter ? "))
+    print("Le montant a payer est de : %f" %())
